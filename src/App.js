73b163a733ed2f9f870e9de7709be0bf2d2b3687
yuraprogrammer <yuraprogrammer1979@gmail.com>
2025-05-09 21:52:06 +0200

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <>
+            This is the header
+            <h1>Welcome to my website</h1>
+        </>
+    );
+}
+
+export default Header;
